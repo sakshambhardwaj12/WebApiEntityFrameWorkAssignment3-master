8c119afa6ca06e92fdbb5e2df4dde4c3966d7607
saksham Upadhyay <saksham.upadhyay@tyutee.com>
2021-04-04 18:28:07 +0530

    Add project files.

---
+++ Migrations/202104041256106_one.cs
@@ -0,0 +1,30 @@
+﻿namespace WebApiEntityFrameWork.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class one : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(nullable: false, maxLength: 20),
+                        QntyInStock = c.Int(nullable: false),
+                        Description = c.String(nullable: false),
+                        Suppiler = c.Int(nullable: false),
+                        DateOfPurchase = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Products");
+        }
+    }
+}
